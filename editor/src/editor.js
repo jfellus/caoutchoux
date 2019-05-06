77748db311b80d8e326f9783a8683796c48e78a6
Jerome Fellus <jerome.fellus@irisa.fr>
2019-05-06 14:12:53 +0200

    ok

--- editor/src/editor.js
+++ editor/src/editor.js
@@ -4,7 +4,7 @@ const fs = require('fs');
 const util = require('./util.js')
 const crypto = require("crypto");
 const child_process = require("child_process")
-
+const hljs = require('./lib/highlight.js')
 
 function setTitle(t) { ipcRenderer.send("title", t) }
 
@@ -25,6 +25,7 @@ function onkeydown(e) {
     else if(e.key == "PageDown") next();
     else if(e.key == 'F5') presentation();
     else if(e.key == 'X') convertTex2SVG("$\\sum_i^n \\left( \\frac{o+p}{z-d} \\right) \\phi $")
+    else if(e.key == 'Enter') p = on_newline(window.getSelection());
     else p = false
   }
 
@@ -35,21 +36,8 @@ function onkeydown(e) {
 
 function oninput(e) {
   console.log(e);
-  if(e.data === '$') {
-    var s = window.getSelection()
-    if(s.rangeCount===1) {
-      var i = s.anchorNode.wholeText.substr(0,s.anchorOffset-1).lastIndexOf('$')
-      if(i!==-1){
-        var after = s.anchorNode.splitText(s.anchorOffset)
-        convertTexNode2SVG(s.anchorNode.splitText(i), () => {
-          window.getSelection().empty()
-          var r = document.createRange()
-          r.setStartBefore(after); r.collapse()
-          window.getSelection().addRange(r)
-        })
-      }
-    }
-  }
+  if(e.data === '$') on_input_latex(window.getSelection())
+  if(e.data === '`') on_input_code(window.getSelection())
 }
 
 isPresentation = false
@@ -114,8 +102,83 @@ function convertTexNode2SVG(node, cb) {
     svg.attr("viewBox", "0 -7 10 9")
     svg.attr("data-tex", tex)
     svg.addClass("latex")
-    span = $("<span><img src='o'></span>")
-    // span.append(svg)
-    $(node).replaceWith(span); cb();
+    $(node).replaceWith(svg); cb();
   })
+}
+
+function convertNodeToCode(node) {
+  console.log(node);
+  var code = node.data;
+  code = $("<pre><code>"+code.substr(1,code.length-2)+"</code></pre>")
+  $(node).replaceWith(code)
+  hljs.highlightBlock(code[0])
+}
+
+//////////////////////
+
+$(function(){
+  toolbar = $("<div class='toolbar'></div>")
+  toolbar.append($("<button>Add block</button>").on("click", ()=>{ add_block() }))
+  $("body").append(toolbar)
+})
+
+function add_block() {
+  $("page[cur]").append("<div class='block'><h2>Title</h2>content</div>")
+}
+
+function setCaretBefore(elt) {
+  window.getSelection().empty()
+  var r = document.createRange()
+  r.setStartBefore(elt); r.collapse()
+  window.getSelection().addRange(r)
+}
+
+function setCaretAtBeginingOf(elt){
+  window.getSelection().empty()
+  var r = document.createRange()
+  r.selectNodeContents(elt);
+  window.getSelection().addRange(r)
+}
+
+
+function on_input_latex(s) {
+  if(s.rangeCount!==1) return;
+  var i = s.anchorNode.wholeText.substr(0,s.anchorOffset-1).lastIndexOf('$')
+  if(i===-1) return;
+  var after = s.anchorNode.splitText(s.anchorOffset)
+  convertTexNode2SVG(s.anchorNode.splitText(i), () => {  setCaretBefore(after)  })
+}
+
+function on_input_code(s) {
+  if(s.rangeCount!==1) return;
+  var i = s.anchorNode.wholeText.substr(0,s.anchorOffset-1).lastIndexOf('`')
+  if(i===-1) return;
+  var after = s.anchorNode.splitText(s.anchorOffset)
+  convertNodeToCode(s.anchorNode.splitText(i))
+  setCaretBefore(after)
+}
+
+function on_newline(s) {
+  if(s.rangeCount!==1) return false;
+
+  console.log($(s.anchorNode).text());
+
+  if($(s.anchorNode).text().indexOf("1. ")===0) return create_ol_at(s.anchorNode)
+  if($(s.anchorNode).text().indexOf("- ")===0) return create_ul_at(s.anchorNode)
+  if($(s.anchorNode).text().indexOf("* ")===0) return create_ul_at(s.anchorNode)
+  return false;
+}
+
+function create_ol_at(node) {
+  e = $("<ol><li>"+$(node).text().substr(3)+"</li><li>&nbsp;</li></ol>")
+  $(node).replaceWith(e)
+  setCaretAtBeginingOf(e.children("li")[1])
+  return true;
+}
+
+function create_ul_at(node) {
+  e = $("<ul><li>"+$(node).text().substr(2)+"</li><li>&nbsp;</li></ul>")
+  $(node).replaceWith(e)
+  setCaretAtBeginingOf(e.children("li")[1])
+  return true;
 }
